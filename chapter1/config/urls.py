166ed6a00a1975cf9b958c74ab9e3a1e4f637fa4
enjore <shawn630730@gmail.com>
2025-04-29 22:50:16 +0700

    django 1일차

--- chapter1/config/urls.py
+++ chapter1/config/urls.py
@@ -21,7 +21,7 @@ from django.contrib import admin
 from django.http import Http404
 from django.shortcuts import render
 from django.urls import path
-from fake_db import user_db
+from chapter1.fake_db import user_db
 
 
 _db = user_db
@@ -29,14 +29,14 @@ _db = user_db
 
 def user_list(request):
     names = [{'id': key, 'name': value['이름']} for key, value in _db.items()]
-    return render(request, 'user_list.html', {'data': names})
+    return render(request, '../../templates/user_list.html', {'data': names})
 
 
 def user_info(request, user_id):
     if user_id > len(_db):
         raise Http404('User not found')
     info = _db[user_id]
-    return render(request, 'user_info.html', {'data': info})
+    return render(request, '../../templates/user_info.html', {'data': info})
 
 
 urlpatterns = [
